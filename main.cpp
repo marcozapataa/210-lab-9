4768c3ee1b13b9a97e60e88ac72f7de1469a956a
Marco <mazapata053@gmail.com>
2026-09-18 16:36:10 -0700

    Milestone 3: read data file contents sequentially into std::array

--- main.cpp
+++ main.cpp
@@ -19,6 +19,27 @@ int main()
     // Read from external data file with 30 elements
     ifstream inputFile("data.txt");
 
+    // Read the data into the STD::array
+    for (int i = 0; i < SIZE; i++)
+    {
+        inputFile >> skyfall[i];
+    }
+    inputFile.close();
+
+    // Display the array has loaded the elements from the file
+    cout << "2. Loaded: " << SIZE << " elements into skyfall." << endl;
+
+    // Display the 15th number using the .at() function
+    cout << "3. The 15th number in the file is " << skyfall.at(14) << endl;
+
+    // Display the elements in the data file
+    cout << "4. Elements:";
+    for (int number : skyfall)
+    {
+        cout << number << " ";
+    }
+    cout << endl;
+
 
     return 0;
 }
