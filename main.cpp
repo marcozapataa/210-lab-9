d9638e472d675bb8c5d50b7c698518a1a942bc7a
Marco <mazapata053@gmail.com>
2026-09-18 16:20:04 -0700

    milestone 1: declare 30-element std::array and check compile time size.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <array>
 
 using namespace std;
 
+const int SIZE = 30;
+
 int main()
 {
+    // Use an STD::array
+    array<int, SIZE> skyfall = {};
+
+    cout << "1. Array size: " << skyfall.size() << endl;
 
 
     return 0;
